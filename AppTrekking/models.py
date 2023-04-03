2cab36d165f3390060306ac528c1f32e586a55cc
Matias Duarte <amatiasduarte@gmail.com>
2023-04-03 18:47:34 -0300

    Preentrega 3-5

--- AppTrekking/models.py
+++ AppTrekking/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Trekking(models.Model):
     nombre = models.TextField(max_length=100)
     estado = models.TextField(max_length=100)
-    creado = models.DateField()
-    modificado = models.DateField()
+    creado = models.DateTimeField(auto_now_add="")
+    modificado = models.DateTimeField(auto_now="")
     
     def terminar(self):
         self.estado = "Terminado"
@@ -26,8 +26,8 @@ class Persona(models.Model):
 class Evento(models.Model):
     nombre = models.TextField(max_length=100)
     estado = models.TextField(max_length=100)
-    creado = models.DateField()
-    modificado = models.DateField()
+    creado = models.DateTimeField(auto_now_add="")
+    modificado = models.DateTimeField(auto_now="")
     
     def concluir(self):
         self.estado = "Concluído"
